1928204dfa259923bf07b5253c18ddec9ab695ce
mike9107 <9107mgarcia@gmail.com>
2023-02-20 02:06:31 -0500

    genre and author models

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, jsonify
 from config.app_config import APP_CONFIG
 from config.db import db
 from config.ma import ma
+from commands.db_cli import db_cli
 from werkzeug.exceptions import HTTPException
 from http.client import BAD_REQUEST, INTERNAL_SERVER_ERROR
-
 from blueprints.books import books_bp
+from blueprints.authors import authors_bp
 from blueprints.ratings import ratings_bp
 from blueprints.comments import comments_bp
-from blueprints.admin   import admin_bp
+from blueprints.admin import admin_bp
 
 app = Flask(__name__)
 app.config.from_mapping(APP_CONFIG)
@@ -17,6 +18,8 @@ ma.init_app(app)
 db.init_app(app)
 
 app.register_blueprint(books_bp)
+app.register_blueprint(books_bp)
+app.register_blueprint(authors_bp)
 app.register_blueprint(ratings_bp)
 app.register_blueprint(comments_bp)
 app.register_blueprint(admin_bp)
