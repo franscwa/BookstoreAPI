bc0e5c6bf427c093a084f444e99920bb00ce1ecc
DagMehxtal7 <dangschool2014@gmail.com>
2023-03-11 19:58:41 -0500

    Updated Wishlist-Management

--- app.py
+++ app.py
@@ -1,33 +1,433 @@
-from flask import Flask, jsonify
-from blueprints.books_bp import books_bp
-from config.app_config import APP_CONFIG
-from config.db import db
-from config.ma import ma
-from werkzeug.exceptions import HTTPException
-from http.client import BAD_REQUEST, INTERNAL_SERVER_ERROR
-from dotenv import load_dotenv
-from models.book import Book
+from flask import Flask, request, jsonify, json, render_template
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import Column, String, Integer, Float
+from flask_marshmallow import Marshmallow
+from marshmallow import Schema,fields
+
+
+#run in Python interpretor
+#from app import db
+#db.create_all()
 
 app = Flask(__name__)
-app.config.from_mapping(APP_CONFIG)
 
-ma.init_app(app)
-db.init_app(app)
+app.app_context().push()
 
-app.register_blueprint(books_bp)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/bookstore'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
 
 
-@app.errorhandler(Exception)
-def default_error(e):
-    if isinstance(e, HTTPException):
-        return jsonify(error=e.description), e.code
-    elif isinstance(e, AssertionError):
-        return jsonify(error=str(e)), BAD_REQUEST
-    return jsonify(error=str(e)), INTERNAL_SERVER_ERROR
+
+
+class Book(db.Model):
+    bookid = Column(Integer(), primary_key = True)
+    title = Column(String(50), nullable = False, unique = True)
+    description = Column(String(100), nullable = False, unique = False)
+    price = Column(Float(), nullable = False, unique = False)
+    
+    
+    def __init__(self, title, description, price):
+        self.title = title
+        self.description = description
+        self.price = price
+    
+    def __repr__(self):
+        return f"<title = {self.title} description = {self.description} price = {self.price}>" 
+    
+    
+    @classmethod
+    def get_all_books(cls):
+        return cls.query.all()
+    
+    @classmethod
+    def get_book(cls, id):
+        return cls.query.get_or_404(id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+
+class BookSchema(Schema):
+    bookid = fields.Integer()
+    title = fields.String()
+    description = fields.String()
+    price = fields.Float()
+
+
+
+
+
+
+class User(db.Model):
+    userid = Column(Integer(), primary_key = True)
+    username = Column(String(25), nullable = False, unique = True)
+    email = Column(String(80), nullable = False, unique = True)
+    
+    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+    
+    def __repr__(self):
+        return f"<username = {self.username} email = {self.email}>" 
+    
+    
+    @classmethod
+    def get_all_users(cls):
+        return cls.query.all()
+    
+    @classmethod
+    def get_user(cls, id):
+        return cls.query.get_or_404(id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        
+    
+class UserSchema(Schema):
+    userid = fields.Integer()
+    username = fields.String()
+    email = fields.String()
+    
+    
+    
+    
+    
+    
+    
+class Wishlist(db.Model):
+    wishlistid = db.Column(db.Integer(), primary_key = True)
+    wishlistname = Column(String(50), nullable = False, unique = False)
+    
+    class WishlistSchema(Schema):
+        wishlistid = fields.Integer()    
+        wishlistname = fields.String()
+        
+        
+    class WishlistBook(db.Model):    
+        bookid = Column(Integer(), primary_key = True)
+        title = Column(String(50), nullable = False, unique = True)
+        description = Column(String(100), nullable = False, unique = False)
+        price = Column(Float(), nullable = False, unique = False)
+    
+    
+        def __init__(self, title, description, price):
+            self.title = title
+            self.description = description
+            self.price = price
+    
+        def __repr__(self):
+            return f"<title = {self.title} description = {self.description} price = {self.price}>" 
+    
+    
+        @classmethod
+        def get_all_books(cls):
+            return cls.query.all()
+    
+        @classmethod
+        def get_book(cls, id):
+            return cls.query.get_or_404(id)
+
+        def save(self):
+            db.session.add(self)
+            db.session.commit()
+
+        def delete(self):
+            db.session.delete(self)
+            db.session.commit()
+
+
+
+    class WishlistBookSchema(Schema):
+        bookid = fields.Integer()
+        title = fields.String()
+        description = fields.String()
+        price = fields.Float()
+            
+        
+
+    
+    
+
+    
+    def __init__(self, wishlistname):
+        self.wishlistname = wishlistname
+
+    
+    def __repr__(self):
+        return f"<wishlistname = {self.wishlistname}>" 
+    
+    
+    @classmethod
+    def get_all_wishlists(cls):
+        return cls.query.all()
+    
+    @classmethod
+    def get_wishlist(cls, id):
+        return cls.query.get_or_404(id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+
+
+
+
+
+
+    
+@app.route('/book', methods = ['POST'])
+def create_a_book():
+    data = request.get_json()
+
+    new_book = Book(
+        name = data.get('title'),
+        description = data.get('description'),
+        price = data.get('price'),
+        
+    )
+
+    new_book.save()
+
+    serializer = BookSchema()
+
+    data = serializer.dump(new_book)
+
+    return jsonify(
+        data
+    ), 201
+
+
+
+
+
+
+@app.route('/book', methods = ['GET'])
+def get_all_books():
+    book = Book.get_all_books()
+
+    serializer = BookSchema(many=True)
+
+    data = serializer.dump(book)
+
+    return jsonify(
+        data
+    )
+
+
+
+@app.route('/book/<int:bookid>', methods = ['GET'])
+def get_book(bookid):
+    book = Book.get_book(bookid)
+
+    serializer = BookSchema()
+
+    data = serializer.dump(book)
+
+    return jsonify(
+        data
+    ), 200
+
+
+
+
+
+
+
+@app.route('/user', methods = ['POST'])
+def create_user():
+    data = request.get_json()
+
+    new_user = User(
+        username = data.get('username'),
+        email = data.get('email'),
+        
+    )
+
+    new_user.save()
+
+    serializer = UserSchema()
+
+    data=serializer.dump(new_user)
+
+    return jsonify(
+        data
+    ), 201
+
+
+
+
+
+@app.route('/user', methods = ['GET'])
+def get_all_users():
+    user = User.get_all_users()
+
+    serializer = UserSchema(many = True)
+
+    data = serializer.dump(user)
+
+    return jsonify(
+        data
+    )
+
+
+
+
+
+@app.route('/user/<int:userid>', methods = ['GET'])
+def get_user(userid):
+    user = User.get_user(userid)
+
+    serializer = UserSchema()
+
+    data = serializer.dump(user)
+
+    return jsonify(
+        data
+    ), 200
+
+
+
+
+
+
+@app.route('/user/<int:userid>', methods = ['PUT'])
+def update_user(userid):
+    user_to_update = User.get_user(userid)
+
+    data = request.get_json()
+
+    user_to_update.username = data.get('username')
+    user_to_update.email = data.get('email')
+    
+
+    db.session.commit()
+
+    serializer = UserSchema()
+
+    user_data = serializer.dump(user_to_update)
+
+    return jsonify(user_data), 200
+
+
+
+
+
+
+@app.route('/wishlist', methods = ['POST'])
+def create_a_wishlist():
+    data = request.get_json()
+
+    new_wishlist = Wishlist(
+        wishlistname = data.get('wishlistname')
+                
+    )
+
+    new_wishlist.save()
+
+    serializer = Wishlist.WishlistSchema()
+
+    data = serializer.dump(new_wishlist)
+
+    return jsonify(
+        data
+    ), 201
+
+
+
+
+
+
+
+@app.route('/wishlist', methods = ['GET'])
+def get_all_wishlists():
+    wishlists = Wishlist.get_all_wishlists()
+
+    serializer = Wishlist.WishlistSchema(many = True)
+
+    data = serializer.dump(wishlists)
+
+    return jsonify(
+        data
+    )
+
+
+
+
+
+@app.route('/wishlist/<int:wishlistid>', methods = ['GET'])
+def get_wishlist(wishlistid):
+    wishlist = Wishlist.get_wishlist(wishlistid)
+
+    serializer = Wishlist.WishlistSchema()
+
+    data = serializer.dump(wishlist)
+
+    return jsonify(
+        data
+    ), 200
+
+
+
+
+"""
+
+@app.route('/wishlist/<int:wishlistid>/wishlistbook', methods = ['POST'])
+def add_a_wishlist_book(wishlistid):
+    
+    wishlist = Wishlist.get_wishlist(wishlistid)
+
+    serializer = Wishlist.WishlistSchema()
+
+    data = serializer.dump(wishlist)
+    
+    
+    data = request.get_json()
+
+    new_wishlist_book = Wishlist.WishlistBook(
+        title = data.get('title'),
+        description = data.get('description'),
+        price = data.get('price')
+        
+    )
+
+    new_wishlist_book.save()
+
+    serializer = WishlistBookSchema()
+
+    data = serializer.dump(new_wishlist_book)
+
+    return jsonify(
+        data
+    ), 200
+
+
+"""
 
 
 def init_db():
-    """Initialize the database."""
+    #Initialize the database.
     with app.app_context():
         db.drop_all()
         db.create_all()
@@ -35,24 +435,38 @@ def init_db():
 
 
 def seed_db():
-    """Seed the database."""
+    #Seed the database.
     with app.app_context():
-        book1 = Book(title="The Hobbit", description="A great book", price=10.99)
-        book2 = Book(title="The Silmarillion", description="A great book", price=11.99)
-        book3 = Book(
-            title="The Fellowship of the Ring", description="A great book", price=12.99
-        )
-        book4 = Book(title="The Two Towers", description="A great book", price=13.99)
-        book5 = Book(
-            title="The Return of the King", description="A great book", price=15.99
-        )
+        book1 = Book(title = "The Hobbit", description = "A great book", price = 10.99)
+        book2 = Book(title = "The Silmarillion", description = "A great book", price = 11.99)
+        book3 = Book(title = "The Fellowship of the Ring", description = "A great book", price = 12.99)
+        book4 = Book(title = "The Two Towers", description = "A great book", price = 13.99)
+        book5 = Book(title = "The Return of the King", description = "A great book", price = 15.99)
         books = [book1, book2, book3, book4, book5]
+        
+        user1 = User(username = "JeremyWal", email = "jewalli@gmail.com")
+        user2 = User(username = "MichaelBer", email = "mberry@gmail.com")
+        users = [user1, user2]
+        
+        wishlist1 = Wishlist(wishlistname = "My upcoming favorites.")
+        wishlist2 = Wishlist(wishlistname = "Book to buy for Mom.")
+        wishlists = [wishlist1, wishlist2]
+        
+        
+        
         db.session.add_all(books)
+        db.session.add_all(users)        
+        db.session.add_all(wishlists)        
         db.session.commit()
+        
     print("database seeded")
-
-
-if __name__ == "__main__":
+    
+ 
+    
+    
+    
+        
+if __name__ == '__main__':
     init_db()
     seed_db()
-    app.run(port=app.config["PORT"])
+    app.run(debug = True)
--- models/book.py
+++
@@ -1,8 +0,0 @@
-from config.db import db
-
-
-class Book(db.Model):
-    book_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200), nullable=False)
-    price = db.Column(db.Float, nullable=False)
