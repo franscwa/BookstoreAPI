e8848a4ead0c7b316bfde889594ad1386df676f9
mike9107 <9107mgarcia@gmail.com>
2023-03-29 00:01:19 -0400

    add unit tests

--- blueprints/books_bp.py
+++ blueprints/books_bp.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, request, jsonify
-from werkzeug.exceptions import NotFound
+from flask import Blueprint, request
+from werkzeug.exceptions import NotFound, BadRequest
 from models.book import Book, book_schema, books_schema
 from models.author import Author
-from http.client import OK, CREATED
+from models.genre import Genre
+from http import HTTPStatus
 from config.db import db
 
 
@@ -14,16 +15,22 @@ def get_book(isbn):
     book = Book.query.get(isbn)
     if book is None:
         raise NotFound(f"Book [isbn={isbn}] not found.")
-    return book_schema.jsonify(book), OK
+    return book_schema.jsonify(book), HTTPStatus.OK
 
 
 @books_bp.post("")
 def create_book():
     book = book_schema.load(request.json)
     book.validate()
+    if book.isbn in {b.isbn for b in Book.query.all()}:
+        raise BadRequest(f"Book [isbn={book.isbn}] already exists.")
+    if book.author_id not in {a.author_id for a in Author.query.all()}:
+        raise NotFound(f"Author [author_id={book.author_id}] not found.")
+    if book.genre_name not in {g.name for g in Genre.query.all()}:
+        raise NotFound(f"Genre [genre_name={book.genre_name}] not found.")
     db.session.add(book)
     db.session.commit()
-    return "", CREATED
+    return "", HTTPStatus.CREATED
 
 
 @books_bp.get("/author/<author_id>")
@@ -31,4 +38,4 @@ def get_books_by_author(author_id):
     author = Author.query.get(author_id)
     if author is None:
         raise NotFound(f"Author [author_id={author_id}] not found.")
-    return books_schema.jsonify(author.books), OK
+    return books_schema.jsonify(author.books), HTTPStatus.OK
