d79f5efca8827f1068dacb35d2efa75b2a3333ec
DagMehxtal7 <dangschool2014@gmail.com>
2023-04-07 20:19:53 -0400

    Separated into different files

---
+++ models/book.py
@@ -0,0 +1,70 @@
+from config.db import db
+from config.ma import ma
+
+
+class Book(db.Model):
+    isbn = db.Column(db.String(13), primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    publisher = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    copies_sold = db.Column(db.Integer, nullable=False, default=0)
+
+    genre_name = db.Column(db.String, db.ForeignKey("genre.name"))
+    genre = db.relationship("Genre", back_populates="books")
+
+    author_id = db.Column(db.Integer, db.ForeignKey("author.author_id"), nullable=False)
+    author = db.relationship("Author", back_populates="books")
+
+    ratings = db.relationship("Rating", back_populates="book")
+
+    comments = db.relationship("Comment", back_populates="book")
+    
+    wishlistid = db.Column(db.Integer, db.ForeignKey('wishlist.wishlistid'))
+
+
+    def validate(self):
+        assert self.isbn is not None and len(self.isbn) == 13, "isbn must be 13 digits"
+        assert (
+            self.title is not None and len(self.title) > 0
+        ), "non-empty book title is required"
+        assert (
+            self.description is not None and len(self.description) > 0
+        ), "non-empty book description is required"
+        assert (
+            self.price is not None and self.price > 0
+        ), "book price must be greater than zero"
+        assert (
+            self.publisher is not None and len(self.publisher) > 0
+        ), "non-empty book publisher is required"
+        assert (
+            self.year is not None and self.year > 0
+        ), "book year must be greater than zero"
+        assert self.copies_sold is None or (
+            self.copies_sold is not None and self.copies_sold >= 0
+        ), "book copies sold must be greater than or equal to zero"
+        assert (
+            self.author_id is not None and self.author_id > 0
+        ), "book author id must be greater than zero"
+        assert (
+            self.genre_name is not None and len(self.genre_name) > 0
+        ), "non-empty book genre name is required"
+        
+    @classmethod
+    def get_isbn(cls, id):
+        return cls.query.get_or_404(id)    
+
+
+class BookSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Book
+        load_instance = True
+        include_fk = True
+        dump_only = ("book_id",)
+        exclude = ("genre", "author", "ratings", "comments")
+        sqla_session = db.session
+
+
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
---
+++ models/user.py
@@ -0,0 +1,53 @@
+from config.db import db
+from config.ma import ma
+from models.role import is_valid_role, get_roles
+
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+    role_name = db.Column(db.String, db.ForeignKey("role.name"), nullable=False)
+    role = db.relationship("Role", back_populates="users")
+    
+    wishlists = db.relationship('Wishlist', backref = 'user')
+
+
+    def validate(self):
+        assert (
+            self.first_name is not None and len(self.first_name) > 0
+        ), "non-empty user first name is required"
+        assert (
+            self.last_name is not None and len(self.last_name) > 0
+        ), "non-empty user last name is required"
+        assert (
+            self.email is not None and len(self.email) > 0
+        ), "non-empty user email is required"
+        assert (
+            self.password is not None and 8 <= len(self.password) <= 32
+        ), "non-empty user password of length [8-32] is required"
+        assert self.role_name is not None and is_valid_role(
+            self.role_name
+        ), f"role_name must be one of {get_roles()}"
+
+
+    @classmethod
+    def get_user(cls, id):
+        return cls.query.get_or_404(id)
+    
+    
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+        include_fk = True
+        dump_only = ("user_id",)
+        exclude = ("role",)
+        sqla_session = db.session
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
